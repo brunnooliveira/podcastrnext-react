14c5bf6413f9bdadc1f4487cfd90d714eec0b652
Brunno <brunnogoncalves@gmail.com>
2021-04-25 13:36:34 -0300

    Definição do favicon
    Title`s das páginas

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,14 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import Image from 'next/image';
-import Link from 'next/link';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './episode.module.scss';
-import { usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -31,6 +33,10 @@ export default function Episode({ episode }) {
   const router = useRouter();
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <button type="button">
           <Link href="/">
